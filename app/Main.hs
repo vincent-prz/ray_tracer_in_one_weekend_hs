e48d20b2a8e4aa10bfad3cfb4ece2e4f36b6fa67
Vincent <perezv815@gmail.com>
2024-04-10 23:11:11 +0200

    2.1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+imageWidth :: Int
+imageWidth = 256
+
+imageHeight :: Int
+imageHeight = 256
+
+colorRatio :: Double
+colorRatio = 255.999
+
+data Color = Color {r :: Int, g :: Int, b :: Int}
+
+getColorString :: Color -> String
+getColorString c = show (r c) ++ " " ++ show (g c) ++ " " ++ show (b c)
+
+getColor :: Int -> Int -> Color
+getColor x y =
+  let red = colorRatio * fromIntegral x / fromIntegral (imageWidth - 1)
+      green = colorRatio * fromIntegral y / fromIntegral (imageHeight - 1)
+      blue = 0
+   in Color {r = round red, g = round green, b = blue}
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStr ("P3\n" ++ show imageWidth ++ " " ++ show imageHeight ++ "\n255\n")
+  let colors = [getColor x y | y <- [0 .. imageHeight - 1], x <- [0 .. imageWidth - 1]]
+  let colorStrings = map getColorString colors
+  mapM_ putStrLn colorStrings
